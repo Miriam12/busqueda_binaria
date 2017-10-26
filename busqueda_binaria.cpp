dd94de52dbfee8e2d33356c0ff78f29421feaab4
Miriam <32524062+Miriam12@users.noreply.github.com>
2017-10-26 13:46:07 -0500

    Add files via upload

---
+++ busqueda_binaria.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int num=0,posicion;
+	int datos[5]={8,12,2,15,4};
+	int temp,valor;
+	bool band=false;
+	
+	
+	
+	
+	printf("Los datos capturados fueron los siguientes: \n");
+	for (int i=0;i<5;i++){
+		 	printf("%i ",datos[i]);
+		 	printf("\n");
+				}
+				
+				printf("\n");
+	for (int i=0; i<4; i++) {
+	  for (int j=i+1; j<5; j++){
+	 	if (datos[i]>datos[j]){
+				temp=datos[i];
+		  		datos[i]=datos[j];
+		   		datos[j]=temp;
+			   }
+		   }
+		}
+				printf("LOS NUMEROS ORDENADOS SON :\n");
+				for(int i=0; i<5; i++){
+					printf("%i ", datos[i]);
+				}		
+		printf("\n");
+		
+	printf("Que numero deseas buscar:")	;
+	scanf("%d",&num);
+	
+	for(int i=0;i<=4;i++){
+					if (datos[i]==num){
+						band=true;
+						posicion=i;
+							}
+					else{
+						band=false;
+					}
+			
+				if (band==true){
+					printf("El valor fue encontrado en la posicion %d. \n", posicion+1);
+				}
+			
+				else{
+					printf("El valor que buscas no existe. \n");
+				}
+			}
+    	system("pause");
+		system("cls");
+	return 0;
+}
